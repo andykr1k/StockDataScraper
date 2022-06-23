9b885bab14439be2759867b6e54693b47061f62d
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-22 18:24:26 -0700

    Fix Info Scraper

--- Scraper.py
+++ Scraper.py
@@ -2,11 +2,9 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
-
 import yfinance as yf
 
-
-#ticker = sys.argv[1]
+ticker = sys.argv[1]
 
 def get_close_price(ticker):
     stock_info = yf.Ticker(ticker).info
@@ -35,31 +33,12 @@ def get_strat(ticker):
 
     return stringStrat
 
-msft = yf.Ticker("MSFT")
+tick = yf.Ticker(ticker)
 
-#stockinfo = msft.info
+stockinfo = tick.info
 
-#for key,value in stockinfo.items():
-    #print(key,":",value)
-
-#numshares = msft.info['sharesOutstanding']
-#print(numshares)
-
-df_dividends = msft.dividends
-df_price = msft.history(period='max')
-#print(df)
-#print(df_price)
-
-data_dividends = df_dividends.resample('Y').sum()
-data_dividends = data_dividends.reset_index()
-data_dividends['Year'] = data_dividends['Date'].dt.year
-plt.figure()
-plt.bar(data_dividends['Year'], data_dividends['Dividends'])
-plt.ylabel('Dividends')
-plt.xlabel('Year')
-plt.title('MSFT Dividends')
-plt.xlim(2001,2021)
-plt.show()
+for key,value in stockinfo.items():
+    print(key,":",value)
 
 #print("Close Price: ", get_close_price(ticker))
 #print("Open Price: ", get_open_price(ticker))
